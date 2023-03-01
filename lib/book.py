a4eacfbf1adbf4da274624f92260b6523f9eefff
Sheena Sang <sheenasany@gmail.com>
2023-03-01 12:51:44 -0500

    updated solution

--- lib/book.py
+++ lib/book.py
@@ -1,4 +1,26 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
+    def __init__(self, title):
+        self.title = title
+        self._page_count = None
+        # _page_count is a property and we make it a property when we want to 
+        # place restrictions on what it can be 
+        # the _ (underscore) denotes it as a property
+        # we set it initialized to None rather than having a default value of 0
+        # since we don't need to have a _page_count property to create an instance of Book
+    
+    def get_page_count(self):
+        return self._page_count
+    
+    def set_page_count(self, page_count):
+        if type(page_count) is int:
+            self._page_count = page_count
+        
+        else:
+            print(f"page_count must be an integer")
+    
+    page_count = property(get_page_count, set_page_count)
+    
+    def turn_page(self):
+        print(f"Flipping the page...wow, you read fast!")
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,26 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self, brand):
+        self.brand = brand
+        self._size = None
+        # _size is a property and we make it a property when we want to 
+        # place restrictions on what it can be 
+        # the _ (underscore) denotes it as a property
+        # we set it initialized to None rather than having a default value of 0
+        # since we don't need to have a _size property to create an instance of Shoe
+    def get_int_size(self):
+        return self._size
+    
+    def set_int_size(self, size):
+        if type(size) is int:
+            self._size = size
+        
+        else:
+            print(f"size must be an integer")
+    
+    size = property(get_int_size, set_int_size)
+    
+    def cobble(self):
+        print(f"Your shoe is as good as new!")
+        self.condition = "New"
